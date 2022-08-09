79322b85a9ff1be610e1a27b56b638fee8d04f97
Andrea <acesparza05@gmail.com>
2022-08-09 11:31:40 -0500

    commit tree acde7f9abde218d0f84a881532eed12e1a3660f0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+    def name
+        @this_dogs_name
+    end
+    # def name=(value)
+    #     @name = value
+    # end
+    # # getter method
+    # def name
+    #     # access the data from the @name instance variable and return it
+    #     @name
+    # end
 end
